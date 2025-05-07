8e2845f94202d3943815bcb4c94110dfa01c7c0f
Matthew Kenyon <matthewkenyon2008@outlook.com>
2025-05-07 14:33:54 +1200

    Add metadata to doc string

--- maze.py
+++ maze.py
@@ -1,33 +1,38 @@
-""" Title:  
-Author:  
-Date: 
-Version:  
-Purpose:    
-""" 
-avatar = (1,2,'A') #row colum Representation
-goal = (1,3,'G') #row colum Representation
-MAZE = [[1,1,1,1,1],[1,0,0,0,1],[1,1,1,1,1]]
-def print_maze(maze,avitar,goal):
-  """ prints out maze to standard io
-      -----
-      - AG-
-      -----
-  """    
-  #add code to print out maze
-  pass
-  
-def move(avi,maze):
-  """ change possition of avitar
-  """   
-  #add code to move  avitar
-  pass
+"""
+Title:  Escalation
+Author:  Matthew Kenyon
+Date:  2025-05-07
+Version:  indev
+Purpose:  Get a engine working for file loading and tracking movement.
+"""
 
-def play_game(avatar,goal,MAZE):
-  """ main game loop
-  """ 
-  while not(avatar[0] == goal[0] and avatar[0] == goal[0]):
+avatar = (1, 2, "A")  # row colum Representation
+goal = (1, 3, "G")  # row colum Representation
+MAZE = [[1, 1, 1, 1, 1], [1, 0, 0, 0, 1], [1, 1, 1, 1, 1]]
+
+
+def print_maze(maze, avitar, goal):
+    """prints out maze to standard io
+    -----
+    - AG-
+    -----
+    """
+    # add code to print out maze
     pass
-  print("congratulations")
-  
+
+
+def move(avi, maze):
+    """change possition of avitar"""
+    # add code to move  avitar
+    pass
+
+
+def play_game(avatar, goal, MAZE):
+    """main game loop"""
+    while not (avatar[0] == goal[0] and avatar[0] == goal[0]):
+        pass
+    print("congratulations")
+
+
 if __name__ == "__main__":
-  play_game(avatar,goal,MAZE)
+    play_game(avatar, goal, MAZE)
