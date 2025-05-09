72d9681b3f267d0480a45ad7a6f6fc96477f7c9a
Matthew Kenyon <matthewkenyon2008@outlook.com>
2025-05-09 12:32:16 +1200

    Create maze-lib.py for file loading

---
+++ maze-lib.py
@@ -0,0 +1,28 @@
+"""
+Title:  Escalation
+Author:  Matthew Kenyon
+Date:  2025-05-07
+Version:  indev
+Purpose:  Get a engine working for file loading and tracking movement.
+"""
+
+
+def load_maze(file):
+    maze_file = open(file, "r")
+    maze_list = []
+    for line in maze_file:
+        maze_list.append(list(line.strip()))
+
+    return maze_list
+
+
+def list_test(rows):
+    maze = []
+    for row in range(rows):
+        maze.append([])
+    print(maze)
+
+
+# code for learning about list of lists
+# list_test(3)
+# list_test(10)
