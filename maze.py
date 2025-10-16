d58c3b4d3729fb80b24e190ba5b69dbd3297dc66
Matthew Kenyon <matthewkenyon2008@outlook.com>
2025-10-17 12:19:25 +1300

    Remove unused variable

--- maze.py
+++ maze.py
@@ -9,7 +9,6 @@ Purpose:  Get a engine working for file loading and tracking movement.
 from maze_lib import load_maze, play_game
 
 maze_file = "./mazes/1.txt"
-avatar = "ඞ"
 avatar_row = 0
 avatar_column = 0
 goal = [2, 3, "G"]  # row colum Representation
