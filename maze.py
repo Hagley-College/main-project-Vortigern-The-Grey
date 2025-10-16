5e277e5f64d8730fafe2dcdd685a1d4f1f7a7990
Matthew Kenyon <matthewkenyon2008@outlook.com>
2025-10-17 12:21:27 +1300

    more comments

--- maze.py
+++ maze.py
@@ -9,10 +9,10 @@ Purpose:  Get a engine working for file loading and tracking movement.
 from maze_lib import load_maze, play_game
 
 maze_file = "./mazes/1.txt"
-avatar_row = 0
+avatar_row = 0  # Initial coordinates of player
 avatar_column = 0
-goal = [2, 3, "G"]  # row colum Representation
-maze = load_maze(maze_file)
+goal = [2, 3, "G"]  # Coordinates and character of goal
+maze = load_maze(maze_file)  # Loads maze fro maze_file
 
 
 if __name__ == "__main__":
